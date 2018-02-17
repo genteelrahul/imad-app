bfb28c2978902004fad90e5d182a6fa59a2f9b4a
genteelrahul <31039217+genteelrahul@users.noreply.github.com>
2018-02-17 16:47:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/artical-three', function (req,res) { res.send('/article-three requested and will be served here');});
+app.get('/artical-two', function (req,res) { res.send('/article-two requested and will be served here');});
+app.get('/artical-one', function (req,res) { res.send('/article-one requested and will be served here');});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
